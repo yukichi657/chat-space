7f2914bfd8f10b2871629f34b6ba9d1569351415
yukichi657 <bnn.otyanhiton423514@icloud.com>
2019-12-22 12:18:55 +0900

    Fix code
    カリキュラムの記載通り、追記
    
    （復習が必要）

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191217080554) do
+ActiveRecord::Schema.define(version: 20191220101207) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string :name,               null: false, unique: true, index: true
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
