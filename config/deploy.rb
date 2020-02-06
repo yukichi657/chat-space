f4b392c220a21a955906724c28f4ff2a1ddd4bb6
yukichi657 <bnn.otyanhiton423514@icloud.com>
2020-02-06 21:05:03 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url,  'git@github.com:yukichi657/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.3'
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
